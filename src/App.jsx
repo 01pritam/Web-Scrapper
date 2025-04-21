ad8b7e12f1a6e2b78dc664034159dfb970c12f65
Pritam Kumar <117884154+01pritam@users.noreply.github.com>
2025-04-21 22:36:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,34 @@ import About from './About';
 
 const App = () => {
   const [product, setProduct] = useState(null);
+  const [scraped, setScraped] = useState(false); 
+
+  const handleScrape = (data) => {
+    setProduct(data);
+    setScraped(true); 
+  };
 
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={
-          <div className="bg-blue-100 min-h-screen flex flex-col items-center justify-center">
-          <div className="container mx-auto px-4 py-8">
-          <p className="text-2xl font-semibold text-gray-600 mt-10 text-center">WELCOME</p>
-            <p className="text-2xl font-semibold text-gray-600 mt-10 text-center">Enjoy Your Smart Shopping Experience Here -</p>
-            <div className="flex flex-wrap items-start justify-center">
-              <div className="w-full md:w-1/3 pr-4 mb-8 md:mb-0">
-                <FormSection setProduct={setProduct} />
-              </div>
-              <div className="w-full md:w-2/3 pl-4">
-                {product && <ProductDetails product={product} />}
+          <div className="bg-blue-100 min-h-screen flex flex-col items-center justify-center transition-all duration-500">
+            <div className="container mx-auto px-4 py-8">
+              <p className="text-2xl font-semibold text-gray-600 mt-10 text-center">WELCOME</p>
+              <p className="text-2xl font-semibold text-gray-600 mt-10 text-center">Enjoy Your Smart Shopping Experience Here -</p>
+
+              <div className={`flex flex-wrap items-start justify-center  transition-all duration-500 ${scraped ? 'md:justify-start' : 'md:justify-center'}`}>
+                <div className={`transition-all duration-500 ${scraped ? 'w-full md:w-1/3' : 'w-full md:w-1/2'} px-4`}>
+                  <FormSection setProduct={handleScrape} />
+                </div>
+
+                <div className={`transition-all duration-500 ease-in-out ${scraped ? 'opacity-100 translate-x-0 w-full md:w-2/3 px-4' : 'opacity-0 -translate-x-10 h-0 overflow-hidden'} transform`}>
+                  {product && <ProductDetails product={product} />}
+                </div>
               </div>
             </div>
           </div>
-        </div>
         } />
         <Route path="/comparison" element={<PriceComparison />} />
         <Route path="/about" element={<About />} />
